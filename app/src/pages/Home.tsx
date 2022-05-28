88f10ffc01fcd387b05cc5d46d9dd6efa0c85ba3
brun0-znx <bruno.chanan01@edu.pucrs.br>
2022-05-28 03:11:20 -0300

    rotas feitas, gremio

---
+++ app/src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import { Header } from "../components/Header";
+import { Posts } from "../components/Posts";
+
+function Home() {
+    return (
+        <div>
+            <Header />
+            <Posts />
+        </div>
+    );
+}
+
+export default Home;
