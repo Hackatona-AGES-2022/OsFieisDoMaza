12b7189afe9dbf8b46c57555bbf493d96908793d
Francisco João Lucca Neto <48070476+FranciscoJLucca@users.noreply.github.com>
2022-05-28 12:28:58 -0300

    Medico repo (#22)
    * atualizado gitignore
    
    * atualizado aluno entidade
    
    * criado aluno-repo
    
    * atualizado endereco repo
    
    * atualizado medico-repo e entidade
    
    * Adicionando repository medico
    
    * Removendo API
    
    * Adicionando api medico
    
    * Adicionando api medico
    
    * API medicos
    
    Co-authored-by: henriquederlam <henriquederlam@gmail.com>
    Co-authored-by: Francisco Joao <francisco.joao.lucca@hp.com>

--- api/src/controllers/medicosController.ts
+++ api/src/controllers/medicosController.ts
@@ -13,6 +13,29 @@ medicoRoute.post("/", async (req, res) => {
     } catch (couldNotCreateException) {
         res.status(400).send(couldNotCreateException.message);
     }
-})
+});
+
+medicoRoute.get("/:id", async (req, res) => {
+    try {
+        const idMedico: string = req.params.id;
+
+        const getMedicoResponse: medicoDocument = await medicoModel.findById({_id: idMedico});
+        if (getMedicoResponse) {
+            res.status(200).json(getMedicoResponse);
+        }
+        res.status(404).json({"Message": "Medico nao registrado."})
+    } catch (couldNotGetMedicoException) {
+        res.status(400).send(couldNotGetMedicoException.message);
+    }
+});
+
+medicoRoute.get("/", async (_req, res) => {
+    try {
+        const medicos: Array<medicoDocument> = await medicoModel.find().limit(3);
+        res.status(200).json(medicos);
+    } catch (couldNotGetMedicosException) {
+        res.status(400).send(couldNotGetMedicosException.message);
+    }
+});
 
 export default medicoRoute;
