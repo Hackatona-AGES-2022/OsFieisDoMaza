a12c22800e96179e8284d4f52f998905a22395c2
Francisco Joao <francisco.joao.lucca@hp.com>
2022-05-28 01:34:20 -0300

    Adiciona estrutura de pastas backend

---
+++ api/entities/Enums/Sexo.ts
@@ -0,0 +1,7 @@
+enum Sexo {
+    Female = 0,
+    Male,
+    Other
+}
+
+export default Sexo;
---
+++ api/entities/Medico.ts
@@ -0,0 +1,8 @@
+import Sexo from "./Enums/Sexo";
+
+class Medico {
+    private sexo:Sexo;
+    private nome:string;
+    private dataNascimento:Date;
+    private cfp:number;
+}
